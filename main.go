5fe598370b34903abdebd909d29dac1bedd829a8
Steve Taranto <staranto@gmail.com>
2025-11-19 05:47:18 -0500

    chore: bump version for release.

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "elements-of-style",
-			Version: "0.0.2",
+			Version: "0.1.0",
 			Rules: []tflint.Rule{
 				rules.NewHungarianRule(),
 				rules.NewLengthRule(),
