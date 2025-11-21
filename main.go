25a19065f8ee78d5bc25c9d8019ffaae7c01a105
Steve Taranto <staranto@gmail.com>
2025-11-21 06:12:28 -0500

    Release version 0.3.4

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "elements-of-style",
-			Version: "0.3.3",
+			Version: "0.3.4",
 			Rules: []tflint.Rule{
 				rules.NewCommentsRule(),
 				rules.NewHungarianRule(),
